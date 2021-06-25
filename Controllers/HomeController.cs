6eae9e553f22a5aae179e0c3eb19ecfb73a688c6
Chris Wong <wc38.wong@gmail.com>
2021-06-25 11:55:26 +0800

    How to export data to Excel in ASP.NET Core 3.0

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ClosedXML.Excel;
 using ExportExcelDemo.Models;
 
 namespace ExportExcelDemo.Controllers
@@ -12,6 +13,13 @@ namespace ExportExcelDemo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
+        List<Author> authors = new List<Author>
+        {
+            new Author { Id = 1, FirstName = "Joydip", LastName = "Kanjilal" },
+            new Author { Id = 2, FirstName = "Steve", LastName = "Smith" },
+            new Author { Id = 3, FirstName = "Anand", LastName = "Narayaswamy"}
+        };
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -32,6 +40,61 @@ namespace ExportExcelDemo.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult DownloadCommaSeperatedFile()
+        {
+            try
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine("Id,FirstName,LastName");
+                foreach (var author in authors)
+                {
+                    stringBuilder.AppendLine($"{author.Id},{author.FirstName},{author.LastName}");
+                }
+                return File(Encoding.UTF8.GetBytes
+                (stringBuilder.ToString()), "text/csv", "authors.csv");
+            }
+            catch
+            {
+                return Error();
+            }
+        }
+
+        public IActionResult DownloadExcelDocument()
+        {
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            string fileName = "authors.xlsx";
+            try
+            {
+                using (var workbook = new XLWorkbook())
+                {
+                    IXLWorksheet worksheet =
+                    workbook.Worksheets.Add("Authors");
+                    worksheet.Cell(1, 1).Value = "Id";
+                    worksheet.Cell(1, 2).Value = "FirstName";
+                    worksheet.Cell(1, 3).Value = "LastName";
+                    for (int index = 1; index <= authors.Count; index++)
+                    {
+                        worksheet.Cell(index + 1, 1).Value =
+                        authors[index - 1].Id;
+                        worksheet.Cell(index + 1, 2).Value =
+                        authors[index - 1].FirstName;
+                        worksheet.Cell(index + 1, 3).Value =
+                        authors[index - 1].LastName;
+                    }
+                    using (var stream = new MemoryStream())
+                    {
+                        workbook.SaveAs(stream);
+                        var content = stream.ToArray();
+                        return File(content, contentType, fileName);
+                    }
+                }
+            }
+            catch
+            {
+                return Error();
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+# [How to export data to Excel in ASP.NET Core 3.0](https://www.infoworld.com/article/3538413/how-to-export-data-to-excel-in-aspnet-core-30.html)
